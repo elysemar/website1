ff38cc85bc1ae07f3c2fc26b1cbb60732fcea872
elysemar <142545977+elysemar@users.noreply.github.com>
2024-02-27 18:00:45 +0000

    Tue, Feb 27, 2024, 10:00 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,7 +1,19 @@
 loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
 
 load = 0
 
-loadText.innerText = `${load}%`
+int = setInterval(blurring, 75)
 
-loadText.style.opacity =
+
+
+function blurring() {
+    load++
+
+    if (load > 99) {
+        clearInterval(int)
+    }
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = 1
+    bg.style.filter = "blur(10px)"
+}
