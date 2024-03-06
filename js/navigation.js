1dbe238abe19ee9bc3c55755703f1e910af2f06a
elysemar <142545977+elysemar@users.noreply.github.com>
2024-03-06 17:06:46 +0000

    Wed, Mar 6, 2024, 9:06 AM -08:00

--- js/navigation.js
+++ js/navigation.js
@@ -4,4 +4,9 @@ window.addEventListener('scroll', fixNav)
 
 function fixNav() {
     console.log(window.scrollY)
+    if(window.scrollY > 223) {
+        nav.classList.add('active')
+    } else {
+        
+    }
 }
