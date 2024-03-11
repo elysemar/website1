efbf2f3a81086f916014f3df6f79dce79134ebae
elysemar <142545977+elysemar@users.noreply.github.com>
2024-03-11 16:54:37 +0000

    Mon, Mar 11, 2024, 9:54 AM -07:00

--- js/landingpage.js
+++ js/landingpage.js
@@ -1,6 +1,11 @@
 toggle = document.getElementById('toggle')
+open = document.getElementById('open')
 
 
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
+})
+
+open.addEventListener('click', () => {
+    modal
 })
