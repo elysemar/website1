9b2a2b6ad135a7cffd7bf7da21703ea270be420e
elysemar <142545977+elysemar@users.noreply.github.com>
2024-03-13 15:56:40 +0000

    Wed, Mar 13, 2024, 8:56 AM -07:00

--- js/landingpage.js
+++ js/landingpage.js
@@ -4,24 +4,21 @@ modal = document.getElementById('modal')
 close = document.getElementById('close')
 
 
-// toggle the nav bar
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
 })
 
-//show the modal
 open.addEventListener('click', () => {
     modal.classList.add('show-modal')
 })
 
-//hide the modal
 close.addEventListener('click', () => {
     modal.classList.remove('show-modal')
 })
 
-//hide the modal on outside click
 window.addEventListener('click', (e) => {
     if (e.target == modal) {
         modal.classList.remove('show-modal')
     }
 }
+)
