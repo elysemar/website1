d857bf0e510926e0dfbe021624c0335083ed19fe
elysemar <142545977+elysemar@users.noreply.github.com>
2024-03-16 06:09:33 +0000

    Fri, Mar 15, 2024, 11:09 PM -07:00

--- js/index.js
+++ js/index.js
@@ -1,13 +1,35 @@
-btn = document.querySelector('.btn')
-search = document.querySelector('.search')
+// btn = document.querySelector('.btn')
+// search = document.querySelector('.search')
+// Lbtn = document.querySelector('.Lbtn')
+// loading = document.querySelector('.loading')
 
-btn.addEventListener('click', () => {
-    search.classList.toggle('active')
-})
+// btn.addEventListener('click', () => {
+//     search.classList.toggle('active')
+// })
 
-Lbtn = document.querySelector('.Lbtn')
-loading = document.querySelector('.loading')
 
-Lbtn.addEventListener('click', () => {
-    loading.classList.toggle('active')
-})
+
+// Lbtn.addEventListener('click', () => {
+//     loading.classList.toggle('active')
+// })
+
+loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
+
+load = 0
+
+int = setInterval(blurring, 50)
+
+
+
+function blurring() {
+    load++
+
+    if (load > 99) {
+        clearInterval(int)
+    }
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = 1 - load/100;
+
+    bg.style.filter = `blur(${30 - (load/100) * 30}px)`
+}
